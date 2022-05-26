e4c19966d81a7e68db3e3f5e9501623d758ddb01
tomoya okamoto <okamoto@inc-works.co.jp>
2022-05-26 18:58:34 +0900

    ユニットテストを、srcフォルダからtestsフォルダに移動させた。

--- tests/math.test.ts
+++ tests/math.test.ts
@@ -1,4 +1,4 @@
-import { add } from "./math";
+import { add } from "../src/helpers/math";
 
 describe("math test", () => {
   test("add", () => {
@@ -6,6 +6,6 @@ describe("math test", () => {
   });
 
   test("add", () => {
-    expect(add(1, 1)).toBe(1);
+    expect(add(1, 1)).toBe(2);
   });
 });
